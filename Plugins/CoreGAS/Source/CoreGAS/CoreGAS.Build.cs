029e42dd93896523dd516cb441a7c7148c93a5b4
VladislavProkopev <vladislavprokopevcplusplus@gmail.com>
2026-04-01 20:31:33 +0500

    Some dependencies added in CoreGAS.Build.cs and ModularProject.Build.cs

--- Plugins/CoreGAS/Source/CoreGAS/CoreGAS.Build.cs
+++ Plugins/CoreGAS/Source/CoreGAS/CoreGAS.Build.cs
@@ -25,7 +25,7 @@ public class CoreGAS : ModuleRules
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
-				"Core",
+				"Core", "GameplayAbilities","GameplayTags","GameplayTasks"
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
--- Source/ModularProject/ModularProject.Build.cs
+++ Source/ModularProject/ModularProject.Build.cs
@@ -8,9 +8,9 @@ public class ModularProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput","NetCore","Iris" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "CoreMovement", "ModularGameplay" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "CoreMovement", "ModularGameplay", "CoreGAS" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
