5fa0c1be938ee77c999f961d49f5fc3366374b6b
VladislavProkopev <vladislavprokopevcplusplus@gmail.com>
2026-04-01 15:15:45 +0500

    Project PawnData created in Project module

---
+++ Source/ModularProject/Character/ProjectPawnData.cpp
@@ -0,0 +1,8 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProjectPawnData.h"
+
+UProjectPawnData::UProjectPawnData(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+}
---
+++ Source/ModularProject/Character/ProjectPawnData.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CoreInputConfig.h"
+#include "ProjectPawnData.generated.h"
+
+#define PROJ_API MODULARPROJECT_API
+/**
+ * 
+ */
+UCLASS(MinimalAPI,BlueprintType,Const,Meta = (DisplayName = "Pawn Data", ShortTooltip = "Data asset used to define a Pawn"))
+class UProjectPawnData : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+public:
+	PROJ_API UProjectPawnData(const FObjectInitializer& ObjectInitializer);
+	
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category= "Project|Pawn")
+	TSubclassOf<APawn> PawnClass;
+	
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category= "Project|Input")
+	TObjectPtr<UCoreInputConfig> InputConfig;
+};
+
+#undef PROJ_API
