64ce6f5f4f065f20591ad0a15ee68067e8588f53
VladislavProkopev <vladislavprokopevcplusplus@gmail.com>
2026-04-01 13:02:09 +0500

    Core Input Config DataAsset Added

---
+++ Plugins/CoreMovement/Source/CoreMovement/Private/CoreInputConfig.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CoreInputConfig.h"
+
+DEFINE_LOG_CATEGORY_STATIC(Log_CoreInputConfig,All,All)
+
+UCoreInputConfig::UCoreInputConfig(const FObjectInitializer& ObjectInitializer)
+{
+}
+
+const UInputAction* UCoreInputConfig::FindNativeInputActionForTag(const FGameplayTag InputTag, bool bLogNotFound) const
+{
+	for (const FCoreInputAction& Action : NativeInputActions)
+	{
+		if (Action.InputAction && Action.InputTag == InputTag)
+		{
+			return Action.InputAction;
+		}
+	}
+
+	if (bLogNotFound)
+	{
+		UE_LOG(Log_CoreInputConfig,Error,TEXT("Can't find NativeInputAction for InputTag [%s] on InputConfig [%s]."),*InputTag.ToString(),*GetNameSafe(this));
+	}
+}
+
+const UInputAction* UCoreInputConfig::FindAbilityInputActionForTag(const FGameplayTag InputTag, bool bLogNotFound) const
+{
+	for (const FCoreInputAction& Action : AbilityInputActions)
+	{
+		if (Action.InputAction && Action.InputTag == InputTag)
+		{
+			return Action.InputAction;
+		}
+	}
+
+	if (bLogNotFound)
+	{
+		UE_LOG(Log_CoreInputConfig,Error,TEXT("Can't find AbilityInputAction for InputTag [%s] on InputConfig [%s]."),*InputTag.ToString(),*GetNameSafe(this));
+	}
+}
---
+++ Plugins/CoreMovement/Source/CoreMovement/Public/CoreInputConfig.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Engine/DataAsset.h"
+#include "GameplayTagContainer.h"
+#include "CoreInputConfig.generated.h"
+
+class UInputAction;
+class UObject;
+class FFrame;
+
+USTRUCT()
+struct FCoreInputAction
+{
+	GENERATED_BODY()
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly)
+	TObjectPtr<const UInputAction> InputAction = nullptr;
+	
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly, Meta = (Categories = "InputTag"))
+	FGameplayTag InputTag;
+	
+};
+
+/**
+ * 
+ */
+UCLASS(BlueprintType,Const)
+class COREMOVEMENT_API UCoreInputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+public:
+	UCoreInputConfig(const FObjectInitializer& ObjectInitializer);
+	
+	UFUNCTION(BlueprintCallable,Category="Core|Pawn")
+	const UInputAction* FindNativeInputActionForTag(const FGameplayTag InputTag, bool bLogNotFound = true) const;
+	
+	UFUNCTION(BlueprintCallable,Category="Core|Pawn")
+	const UInputAction* FindAbilityInputActionForTag(const FGameplayTag InputTag, bool bLogNotFound = true) const;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Meta = (TitleProperty = "InputAction"))
+	TArray<FCoreInputAction> NativeInputActions;
+	
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly, Meta = (TitleProperty = "InputAction"))
+	TArray<FCoreInputAction> AbilityInputActions;
+};
