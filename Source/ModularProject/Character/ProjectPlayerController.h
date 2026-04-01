5d3fe49414bdb4180c158e83b9e7f51a5929ca0c
VladislavProkopev <vladislavprokopevcplusplus@gmail.com>
2026-04-01 15:16:47 +0500

    ProjectPlayerController created in Project Module

---
+++ Source/ModularProject/Character/ProjectPlayerController.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ProjectPlayerController.generated.h"
+
+class UInputMappingContext;
+/**
+ * 
+ */
+UCLASS()
+class MODULARPROJECT_API AProjectPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	virtual void SetupInputComponent() override;
+	
+private:
+	
+};
