d763a1e148123d7e824aee1be09eb5ff73317299
VladislavProkopev <vladislavprokopevcplusplus@gmail.com>
2026-04-01 15:14:41 +0500

    Base Native GameplayTags Created in ModularCore plugin

---
+++ Plugins/ModularCore/Source/ModularCore/Private/CoreGameplayTags.cpp
@@ -0,0 +1,15 @@
+﻿#include "CoreGameplayTags.h"
+
+namespace CoreGameplayTags
+{
+	namespace NativeInput
+	{
+		UE_DEFINE_GAMEPLAY_TAG(Input_Move, "NativeInput.Move");
+		UE_DEFINE_GAMEPLAY_TAG(Input_Look, "NativeInput.Look");
+	}
+
+	namespace Abilities
+	{
+		UE_DEFINE_GAMEPLAY_TAG(Ability_TEST, "Abilities.TEST");
+	}
+}
---
+++ Plugins/ModularCore/Source/ModularCore/Public/CoreGameplayTags.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "NativeGameplayTags.h"
+
+namespace CoreGameplayTags
+{
+	namespace NativeInput
+	{
+		MODULARCORE_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Input_Move);
+		MODULARCORE_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Input_Look);
+	}
+
+	namespace Abilities
+	{
+		MODULARCORE_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_TEST);
+	}
+}
