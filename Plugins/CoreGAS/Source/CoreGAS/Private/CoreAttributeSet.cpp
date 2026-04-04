cf17d7c96b06f640a35fed4ce3d2955dd0333827
VladislavProkopev <vladislavprokopevcplusplus@gmail.com>
2026-04-05 02:26:25 +0500

    CoreAttributeSet.h Base Created

---
+++ Plugins/CoreGAS/Source/CoreGAS/Private/CoreAttributeSet.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CoreAttributeSet.h"
+#include "CoreAbilitySystemComponent.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(CoreAttributeSet)
+
+class UWorld;
+
+UCoreAttributeSet::UCoreAttributeSet()
+{
+}
+
+UWorld* UCoreAttributeSet::GetWorld() const
+{
+	const UObject* Outer = GetOuter();
+	check(Outer);
+	
+	return Outer->GetWorld();
+}
+
+UCoreAbilitySystemComponent* UCoreAttributeSet::GetCoreAbilitySystemComponent() const
+{
+	return Cast<UCoreAbilitySystemComponent>(GetOwningAbilitySystemComponent());
+}
---
+++ Plugins/CoreGAS/Source/CoreGAS/Public/CoreAttributeSet.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "CoreAttributeSet.generated.h"
+
+class AActor;
+class UCoreAbilitySystemComponent;
+class UObject;
+class UWorld;
+struct FGameplayEffectSpec;
+
+#define GAS_API COREGAS_API
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+/**
+ * 
+ */
+UCLASS(MinimalAPI)
+class COREGAS_API UCoreAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+public:
+	GAS_API UCoreAttributeSet();
+	
+	GAS_API UWorld* GetWorld() const override;
+	
+	GAS_API UCoreAbilitySystemComponent* GetCoreAbilitySystemComponent() const;
+};
+
+#undef GAS_API
