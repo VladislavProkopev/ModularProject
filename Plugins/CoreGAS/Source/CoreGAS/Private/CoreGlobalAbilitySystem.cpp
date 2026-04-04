f094400326322426db39bd3e64275a6b85c232c0
VladislavProkopev <vladislavprokopevcplusplus@gmail.com>
2026-04-04 22:02:54 +0500

    CoreGlobalAbilitySystem.h CoreGlobalAbilitySystem.cpp Base Added

---
+++ Plugins/CoreGAS/Source/CoreGAS/Private/CoreGlobalAbilitySystem.cpp
@@ -0,0 +1,175 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CoreGlobalAbilitySystem.h"
+#include "CoreAbilitySystemComponent.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(CoreGlobalAbilitySystem)
+
+void FGlobalAppliedAbilityList::AddToASC(TSubclassOf<UGameplayAbility> Ability, UCoreAbilitySystemComponent* ASC)
+{
+	checkf(ASC,TEXT("ASC in FGlobalAppliedAbilityList::AddToASC - invalid or null"));
+	checkf(Ability,TEXT("Ability in FGlobalAppliedAbilityList::AddToASC - invalid or null"));
+	
+	FObjectKey ASCKey(ASC);
+	
+	if (Handles.Contains(ASCKey))
+	{
+		RemoveFromASC(ASC);
+	}
+	
+	UGameplayAbility* AbilityCDO = Ability->GetDefaultObject<UGameplayAbility>();
+	FGameplayAbilitySpec AbilitySpec(AbilityCDO);
+	const FGameplayAbilitySpecHandle AbilitySpecHandle = ASC->GiveAbility(AbilitySpec);
+	Handles.Add(ASCKey,AbilitySpecHandle);
+}
+
+void FGlobalAppliedAbilityList::RemoveFromASC(UCoreAbilitySystemComponent* ASC)
+{
+	checkf(ASC,TEXT("ASC in FGlobalAppliedAbilityList::RemoveFromASC - invalid or null"));
+	
+	FObjectKey ASCKey(ASC);
+	if (FGameplayAbilitySpecHandle* SpecHandle = Handles.Find(ASCKey))
+	{
+		ASC->ClearAbility(*SpecHandle);
+		Handles.Remove(ASCKey);
+	}
+}
+
+void FGlobalAppliedAbilityList::RemoveFromAll()
+{
+	for (auto& KVP : Handles)
+	{
+		// Пытаемся восстановить оригинальный указатель из FObjectKey.
+		// Если объект был уничтожен в обход UnregisterASC, это безопасно вернет nullptr.
+		if (UCoreAbilitySystemComponent* ASC = Cast<UCoreAbilitySystemComponent>(KVP.Key.ResolveObjectPtr()))
+		{
+			ASC->ClearAbility(KVP.Value);
+		}
+	}
+	Handles.Empty();
+}
+
+void FGlobalAppliedEffectList::AddToASC(TSubclassOf<UGameplayEffect> Effect, UCoreAbilitySystemComponent* ASC)
+{
+	if (FActiveGameplayEffectHandle* Handle = Handles.Find(ASC))
+	{
+		RemoveFromASC(ASC);
+	}
+	
+	const UGameplayEffect* EffectCDO = Effect->GetDefaultObject<UGameplayEffect>();
+	const FActiveGameplayEffectHandle GameplayEffectHandle = ASC->ApplyGameplayEffectToSelf(EffectCDO,1,ASC->MakeEffectContext());
+	Handles.Add(ASC,GameplayEffectHandle);
+}
+
+void FGlobalAppliedEffectList::RemoveFromASC(UCoreAbilitySystemComponent* ASC)
+{
+	if (FActiveGameplayEffectHandle* Handle = Handles.Find(ASC))
+	{
+		ASC->RemoveActiveGameplayEffect(*Handle);
+		Handles.Remove(ASC);
+	}
+}
+
+void FGlobalAppliedEffectList::RemoveFromAll()
+{
+	for (auto& KVP : Handles)
+	{
+		// Пытаемся восстановить оригинальный указатель из FObjectKey.
+		// Если объект был уничтожен в обход UnregisterASC, это безопасно вернет nullptr.
+		if (UCoreAbilitySystemComponent* ASC = Cast<UCoreAbilitySystemComponent>(KVP.Key.ResolveObjectPtr()))
+		{
+			ASC->RemoveActiveGameplayEffect(KVP.Value);
+		}
+	}
+	Handles.Empty();
+}
+
+void UCoreGlobalAbilitySystem::ApplyAbilityToAll(TSubclassOf<UGameplayAbility> Ability)
+{
+	if (Ability && !AppliedAbilities.Contains(Ability))
+	{
+		FGlobalAppliedAbilityList& Entry = AppliedAbilities.Add(Ability);
+		
+		for (const TWeakObjectPtr<UCoreAbilitySystemComponent>& WeakASC : RegisteredASCs)
+		{
+			if (UCoreAbilitySystemComponent* ASC = WeakASC.Get())
+			{
+				Entry.AddToASC(Ability,ASC);
+			}
+		}
+	}
+}
+
+void UCoreGlobalAbilitySystem::ApplyEffectToAll(TSubclassOf<UGameplayEffect> Effect)
+{
+	if ((Effect.Get() != nullptr)&&(!AppliedEffects.Contains(Effect)))
+	{
+		FGlobalAppliedEffectList& Entry = AppliedEffects.Add(Effect);
+		for (const TWeakObjectPtr<UCoreAbilitySystemComponent>& WeakASC : RegisteredASCs)
+		{
+			if (UCoreAbilitySystemComponent* ASC = WeakASC.Get())
+			{
+				Entry.AddToASC(Effect,ASC);
+			}
+		}
+	}
+}
+
+void UCoreGlobalAbilitySystem::RemoveAbilityFromAll(TSubclassOf<UGameplayAbility> Ability)
+{
+	if ((Ability.Get() != nullptr)&& AppliedAbilities.Contains(Ability))
+	{
+		FGlobalAppliedAbilityList& Entry = AppliedAbilities[Ability];
+		Entry.RemoveFromAll();
+		AppliedAbilities.Remove(Ability);
+	}
+}
+
+void UCoreGlobalAbilitySystem::RemoveEffectFromAll(TSubclassOf<UGameplayEffect> Effect)
+{
+	if ((Effect.Get() != nullptr)&& AppliedEffects.Contains(Effect))
+	{
+		FGlobalAppliedEffectList& Entry = AppliedEffects[Effect];
+		Entry.RemoveFromAll();
+		AppliedEffects.Remove(Effect);
+	}
+}
+
+void UCoreGlobalAbilitySystem::RegisterASC(UCoreAbilitySystemComponent* ASC)
+{
+	checkf(ASC,TEXT("ASC in UCoreGlobalAbilitySystem::RegisterASC - invalid or null"));
+	
+	// Предполагается, что компонент вызывает Register 1 раз на BeginPlay.
+	RegisteredASCs.Add(ASC);
+	
+	for (auto& Entry : AppliedAbilities)
+	{
+		Entry.Value.AddToASC(Entry.Key,ASC);
+	}
+	for (auto& Entry : AppliedEffects)
+	{
+		Entry.Value.AddToASC(Entry.Key,ASC);
+	}
+	
+	/* Возможно использование AddUnique - дороже но не нужно контролировать разовую инициализацию
+	RegisteredASCs.AddUnique(ASC);
+	*/
+}
+
+void UCoreGlobalAbilitySystem::UnregisterASC(UCoreAbilitySystemComponent* ASC)
+{
+	checkf(ASC,TEXT("ASC in UCoreGlobalAbilitySystem::UnregisterASC - invalid or null"));
+	
+	for (auto& Entry : AppliedAbilities)
+	{
+		Entry.Value.RemoveFromASC(ASC);
+	}
+	for (auto& Entry : AppliedEffects)
+	{
+		Entry.Value.RemoveFromASC(ASC);
+	}
+	// Swap-удаление. Это O(1) вместо O(N), так как не сдвигает весь массив,
+	// а просто меняет удаляемый элемент с последним. Порядок в RegisteredASCs нам не важен.
+	RegisteredASCs.RemoveSwap(ASC);
+}
---
+++ Plugins/CoreGAS/Source/CoreGAS/Public/CoreGlobalAbilitySystem.h
@@ -0,0 +1,79 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ActiveGameplayEffectHandle.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "Templates/SubclassOf.h"
+#include "GameplayAbilitySpecHandle.h"
+#include "UObject/ObjectKey.h"
+#include "CoreGlobalAbilitySystem.generated.h"
+
+class UGameplayAbility;
+class UGameplayEffect;
+class UCoreAbilitySystemComponent;
+class UObject;
+struct FActiveGameplayEffectHandle;
+struct FFrame;
+struct FGameplayAbilitySpecHandle;
+
+USTRUCT()
+struct FGlobalAppliedAbilityList
+{
+	GENERATED_BODY()
+	
+	TMap<FObjectKey,FGameplayAbilitySpecHandle> Handles;
+	
+	void AddToASC(TSubclassOf<UGameplayAbility> Ability,UCoreAbilitySystemComponent* ASC);
+	void RemoveFromASC(UCoreAbilitySystemComponent* ASC);
+	void RemoveFromAll();
+	
+};
+
+USTRUCT()
+struct FGlobalAppliedEffectList
+{
+	GENERATED_BODY()
+	
+	TMap<FObjectKey,FActiveGameplayEffectHandle> Handles;
+	
+	void AddToASC(TSubclassOf<UGameplayEffect> Effect,UCoreAbilitySystemComponent* ASC);
+	void RemoveFromASC(UCoreAbilitySystemComponent* ASC);
+	void RemoveFromAll();
+};
+/**
+ * 
+ */
+UCLASS()
+class COREGAS_API UCoreGlobalAbilitySystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable,BlueprintAuthorityOnly,Category=CoreGAS)
+	void ApplyAbilityToAll(TSubclassOf<UGameplayAbility> Ability);
+	
+	UFUNCTION(BlueprintCallable,BlueprintAuthorityOnly,Category=CoreGAS)
+	void ApplyEffectToAll(TSubclassOf<UGameplayEffect> Effect);
+	
+	UFUNCTION(BlueprintCallable,BlueprintAuthorityOnly,Category=CoreGAS)
+	void RemoveAbilityFromAll(TSubclassOf<UGameplayAbility> Ability);
+	
+	UFUNCTION(BlueprintCallable,BlueprintAuthorityOnly,Category=CoreGAS)
+	void RemoveEffectFromAll(TSubclassOf<UGameplayEffect> Effect);
+	
+	/** Register an ASC with global system and apply any active global effects/abilities. */
+	void RegisterASC(UCoreAbilitySystemComponent* ASC);
+	
+	/** Removes an ASC from the global system, along with any active global effects/abilities. */
+	void UnregisterASC(UCoreAbilitySystemComponent* ASC);
+	
+private:
+	UPROPERTY()
+	TMap<TSubclassOf<UGameplayAbility>,FGlobalAppliedAbilityList> AppliedAbilities;
+	
+	UPROPERTY()
+	TMap<TSubclassOf<UGameplayEffect>,FGlobalAppliedEffectList> AppliedEffects;
+	
+	// UPROPERTY здесь не нужен, так как TWeakObjectPtr не сериализуется движком по умолчанию таким образом.
+	TArray<TWeakObjectPtr<UCoreAbilitySystemComponent>> RegisteredASCs;
+};
