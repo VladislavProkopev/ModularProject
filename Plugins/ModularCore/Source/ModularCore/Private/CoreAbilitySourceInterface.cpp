a5c68d3f94c1886a92408c0a8d7713f428a0ddb7
VladislavProkopev <vladislavprokopevcplusplus@gmail.com>
2026-04-04 22:01:05 +0500

    CoreAbilitySourceInterface.h Base Added

---
+++ Plugins/ModularCore/Source/ModularCore/Private/CoreAbilitySourceInterface.cpp
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CoreAbilitySourceInterface.h"
+
+
+// Add default functionality here for any ICoreAbilitySourceInterface functions that are not pure virtual.
---
+++ Plugins/ModularCore/Source/ModularCore/Public/CoreAbilitySourceInterface.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "CoreAbilitySourceInterface.generated.h"
+
+class UObject;
+class UPhysicalMaterial;
+struct FGameplayTagContainer;
+
+UINTERFACE(MinimalAPI, BlueprintType)
+class UCoreAbilitySourceInterface : public UInterface
+{
+	GENERATED_BODY()
+	
+};
+
+/**
+ * 
+ */
+class ICoreAbilitySourceInterface
+{
+	GENERATED_BODY()
+
+	/**
+	 * Compute the multiplier for effect falloff with distance
+	 * 
+	 * @param Distance			Distance from source to target for ability calculations (distance bullet traveled for a gun, etc...)
+	 * @param SourceTags		Aggregated Tags from the source
+	 * @param TargetTags		Aggregated Tags currently on the target
+	 * 
+	 * @return Multiplier to apply to the base attribute value due to distance
+	 */
+public:
+	
+	virtual float GetDistanceAttenuation(float Distance, const FGameplayTagContainer* SourceTags = nullptr, const FGameplayTagContainer* TargetTags = nullptr)const = 0;
+	
+	virtual float GetPhysicalMaterialAttenuation(const UPhysicalMaterial* PhysicalMaterial, const FGameplayTagContainer* SourceTags = nullptr, const FGameplayTagContainer* TargetTags = nullptr)const = 0;
+};
