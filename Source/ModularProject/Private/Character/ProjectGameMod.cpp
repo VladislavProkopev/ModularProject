e24e69685bc825a7e63d79411e6efea2e08f1cdc
VladislavProkopev <vladislavprokopevcplusplus@gmail.com>
2026-04-05 02:27:52 +0500

    ProjectGameMod.h Base Created

---
+++ Source/ModularProject/Private/Character/ProjectGameMod.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ModularProject/Public/Character/ProjectGameMod.h"
+#include "Engine/AssetManager.h"
+#include "Tasks/Task.h"
+
+void AProjectGameMod::PreloadPawnData(TSoftObjectPtr<UProjectPawnData> SoftPawnData)
+{
+	if (SoftPawnData.IsNull()) return;
+	
+	// Выносим тяжелую загрузку (если граф зависимостей ассета большой)
+	UE::Tasks::FTask LoadTask = UE::Tasks::Launch(UE_SOURCE_LOCATION,[SoftPawnData]()
+	{
+		// StreamableManager асинхронно подгружает ассет в память
+		UAssetManager::GetStreamableManager().LoadSynchronous(SoftPawnData.ToSoftObjectPath());
+	}, UE::Tasks::ETaskPriority::BackgroundHigh);
+	
+	/*TODO
+	Продолжаем логику спавна только когда таска завершена
+	В реальном проекте мы привязываем Delegate к завершению State Machine загрузки игрока
+	*/
+}
---
+++ Source/ModularProject/Public/Character/ProjectGameMod.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectGameMod.generated.h"
+
+class UProjectPawnData;
+/**
+ * 
+ */
+UCLASS()
+class MODULARPROJECT_API AProjectGameMod : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+private:
+	void PreloadPawnData(TSoftObjectPtr<UProjectPawnData> SoftPawnData);
+};
