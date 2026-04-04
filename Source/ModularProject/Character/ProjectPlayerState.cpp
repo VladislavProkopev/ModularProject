cf1906960ae5be32fcd50db68e09e65b4df5aad5
VladislavProkopev <vladislavprokopevcplusplus@gmail.com>
2026-04-05 02:30:24 +0500

    ProjectPlayerState.h Base Created

---
+++ Source/ModularProject/Character/ProjectPlayerState.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProjectPlayerState.h"
+#include "CoreAbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
+
+AProjectPlayerState::AProjectPlayerState(const FObjectInitializer ObjectInitializer) : Super(ObjectInitializer)
+{
+	AbilitySystemComponent = CreateDefaultSubobject<UCoreAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+	NetUpdateFrequency = 100.f;
+}
+
+UAbilitySystemComponent* AProjectPlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
---
+++ Source/ModularProject/Character/ProjectPlayerState.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AbilitySystemInterface.h"
+#include "ProjectPlayerState.generated.h"
+
+class UCoreAbilitySystemComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class MODULARPROJECT_API AProjectPlayerState : public APlayerState , public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+public:
+	AProjectPlayerState(const FObjectInitializer ObjectInitializer = FObjectInitializer::Get());
+	
+	//~ Begin IAbilitySystemInterface
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	//~ End IAbilitySystemInterface
+	
+protected:
+	UPROPERTY(VisibleAnywhere,Category="Core|PlayerState")
+	TObjectPtr<UCoreAbilitySystemComponent> AbilitySystemComponent;
+};
