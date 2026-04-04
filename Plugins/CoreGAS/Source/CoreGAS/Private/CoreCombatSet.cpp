ccd6c7ed437c5e14f9fd8c5c57aeb05706bdac03
VladislavProkopev <vladislavprokopevcplusplus@gmail.com>
2026-04-05 02:26:50 +0500

    CoreCombatSet.h Base Created

---
+++ Plugins/CoreGAS/Source/CoreGAS/Private/CoreCombatSet.cpp
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CoreCombatSet.h"
+
+#include "Net/UnrealNetwork.h"
+
+UCoreCombatSet::UCoreCombatSet()
+{
+}
+
+void UCoreCombatSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	FDoRepLifetimeParams Params;
+	Params.bIsPushBased = true;
+	Params.Condition = COND_OwnerOnly;
+	Params.RepNotifyCondition = REPNOTIFY_Always;
+	
+	DOREPLIFETIME_WITH_PARAMS_FAST(ThisClass,BaseDamage,Params);
+	DOREPLIFETIME_WITH_PARAMS_FAST(ThisClass,Health,Params);
+	DOREPLIFETIME_WITH_PARAMS_FAST(ThisClass,MaxHealth,Params);
+	DOREPLIFETIME_WITH_PARAMS_FAST(ThisClass,Shield,Params);
+	DOREPLIFETIME_WITH_PARAMS_FAST(ThisClass,MaxShield,Params);
+}
+
+void UCoreCombatSet::OnRep_BaseDamage(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass,BaseDamage,OldValue);
+}
+
+void UCoreCombatSet::OnRep_Health(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass,Health,OldValue);
+}
+
+void UCoreCombatSet::OnRep_MaxHealth(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass,MaxHealth,OldValue);
+}
+
+void UCoreCombatSet::OnRep_Shield(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass,Shield,OldValue);
+}
+
+void UCoreCombatSet::OnRep_MaxShield(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass,MaxShield,OldValue);
+}
---
+++ Plugins/CoreGAS/Source/CoreGAS/Public/CoreCombatSet.h
@@ -0,0 +1,62 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "AbilitySystemComponent.h"
+#include "CoreAttributeSet.h"
+#include "CoreCombatSet.generated.h"
+
+class UObject;
+struct FFrame;
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class COREGAS_API UCoreCombatSet : public UCoreAttributeSet
+{
+	GENERATED_BODY()
+	
+public:
+	UCoreCombatSet();
+	
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	ATTRIBUTE_ACCESSORS(ThisClass,BaseDamage);
+	ATTRIBUTE_ACCESSORS(ThisClass,Health);
+	ATTRIBUTE_ACCESSORS(ThisClass,MaxHealth);
+	ATTRIBUTE_ACCESSORS(ThisClass,Shield);
+	ATTRIBUTE_ACCESSORS(ThisClass,MaxShield);
+	
+protected:
+	UFUNCTION()
+	void OnRep_BaseDamage(const FGameplayAttributeData& OldValue);
+	
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldValue);
+	
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldValue);
+	
+	UFUNCTION()
+	void OnRep_Shield(const FGameplayAttributeData& OldValue);
+	
+	UFUNCTION()
+	void OnRep_MaxShield(const FGameplayAttributeData& OldValue);
+	
+private:
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_BaseDamage,Category="GAS|CombatAttributeSet",Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData BaseDamage;
+	
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_BaseDamage,Category="GAS|CombatAttributeSet",Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData Health;
+	
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_BaseDamage,Category="GAS|CombatAttributeSet",Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData MaxHealth;
+	
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_BaseDamage,Category="GAS|CombatAttributeSet",Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData Shield;
+	
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_BaseDamage,Category="GAS|CombatAttributeSet",Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData MaxShield;
+};
