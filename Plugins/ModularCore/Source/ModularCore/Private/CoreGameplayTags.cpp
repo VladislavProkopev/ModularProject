24b5d698f5706557061516aea32f98a7e0eb4876
VladislavProkopev <vladislavprokopevcplusplus@gmail.com>
2026-04-16 01:36:48 +0500

    CoreGameplayTags.h InitStates added

--- Plugins/ModularCore/Source/ModularCore/Private/CoreGameplayTags.cpp
+++ Plugins/ModularCore/Source/ModularCore/Private/CoreGameplayTags.cpp
@@ -12,5 +12,14 @@ namespace CoreGameplayTags
 	{
 		UE_DEFINE_GAMEPLAY_TAG(Ability_TEST, "Abilities.TEST");
 		UE_DEFINE_GAMEPLAY_TAG(Ability_AbilityInputBlocked, "Abilities.AbilityInputBlocked");
+		UE_DEFINE_GAMEPLAY_TAG(Ability_Behavior_SurvivesDeath, "Abilities.Behaviour.SurvivesDeath");
+	}
+
+	namespace GameFeaturesDataStates
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(InitState_Spawned,"InitState.Spawned", "1: Actor/component has initially spawned and can be extented");
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(InitState_DataAvaliable,"InitState.DataAvaliable","2: All required data has been loaded/replicated and is ready for initialization");
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(InitState_DataInitialized,"InitState.DataInitialized","3: The avaliable data has been initialized for this actor/component, but it is not ready for fuul gameplay");
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(InitState_GameplayReady,"InitState.GameplayReady","4: The actor/component is fully ready for active gameplay");
 	}
 }
