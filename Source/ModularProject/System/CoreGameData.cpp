b46381b1d3267c8832e3437b6395efa177db9c02
VladislavProkopev <vladislavprokopevcplusplus@gmail.com>
2026-04-18 00:56:36 +0500

    CoreGameData.cpp Base created

---
+++ Source/ModularProject/System/CoreGameData.cpp
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CoreGameData.h"
+#include "CoreAssetManager.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(CoreGameData)
+
+UCoreGameData::UCoreGameData()
+{
+}
+
+const UCoreGameData& UCoreGameData::Get()
+{
+	return UCoreAssetManager::Get().GetGameData();
+}
---
+++ Source/ModularProject/System/CoreGameData.h
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Engine/DataAsset.h"
+#include "CoreGameData.generated.h"
+
+#define MOD_API MODULARPROJECT_API
+
+struct FGameplayTag;
+class UGameplayEffect;
+class UObject;
+
+/**
+ * UCoreGameData
+ * 
+ * Non-Mutable data asset that contains global game data
+ */
+
+UCLASS(MinimalAPI,BlueprintType,Const, Meta = (DisplayName = "Core GameData",ShortTooltip = "Data asset containing global game data."))
+class UCoreGameData : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	MOD_API UCoreGameData();
+	
+	//Returns the loaded game data
+	static MOD_API const UCoreGameData& Get();
+	
+	//Gameplay effect used to apply damage. Uses SetByCaller for the damage magnitude.
+	UPROPERTY(EditDefaultsOnly,Category="Default Gameplay Effects", Meta = (DisplayName = "Damage Gameplay Effect (SetByCaller)"))
+	TSoftClassPtr<UGameplayEffect> DamageGameplayEffect_SetByCaller;
+	
+	//Gameplay effect used to apply healing. Uses SetByCaller for the healing magnitude.
+	UPROPERTY(EditDefaultsOnly,Category="Default Gameplay Effects", Meta = (DisplayName = "Heal Gameplay Effects (SetByCaller)"))
+	TSoftClassPtr<UGameplayEffect> HealGameplayEffect_SetByCaller;
+	
+	//Gameplay effect used to add and remove dynamic tags.
+	UPROPERTY(EditDefaultsOnly,Category="Default Gameplay Effects")
+	TSoftClassPtr<UGameplayEffect> DynamicTagGameplayEffect;
+	
+	UPROPERTY(EditDefaultsOnly,Category="Gameplay Cues", Meta = (Categories = "Gameplay Cues"))
+	TArray<FGameplayTag> AlwaysLoadedGameplayCues;
+};
+
+#undef MOD_API
