e2118f4203be0db07177589f2b987af879653d79
VladislavProkopev <vladislavprokopevcplusplus@gmail.com>
2026-04-18 00:56:04 +0500

    CoreAssetManagerStartupJob.cpp base created

---
+++ Source/ModularProject/System/CoreAssetManagerStartupJob.cpp
@@ -0,0 +1,24 @@
+﻿#include "CoreAssetManagerStartupJob.h"
+//TODO подумать нужны ли LogChannels
+
+DEFINE_LOG_CATEGORY_STATIC(Log_CoreAssetManager,All,All);
+
+TSharedPtr<FStreamableHandle> FCoreAssetManagerStartupJob::DoJob() const
+{
+	const double JobStartTime = FPlatformTime::Seconds();
+	
+	TSharedPtr<FStreamableHandle> Handle;
+	UE_LOG(Log_CoreAssetManager,Display,TEXT("Startup job \"%s\" starting"),*JobName);
+	JobFunc(*this,Handle);
+
+	if (Handle.IsValid())
+	{
+		Handle->BindUpdateDelegate(FStreamableUpdateDelegate::CreateRaw(this,&FCoreAssetManagerStartupJob::UpdateSubstepProgressFromStreamable));
+		Handle->WaitUntilComplete(0.0f,false);
+		Handle->BindUpdateDelegate(FStreamableUpdateDelegate());
+	}
+	
+	UE_LOG(Log_CoreAssetManager,Display,TEXT("Startup job \"%s\" took %.2f seconds to complete"),*JobName,FPlatformTime::Seconds() - JobStartTime);
+	
+	return Handle;
+}
---
+++ Source/ModularProject/System/CoreAssetManagerStartupJob.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include "Engine/StreamableManager.h"
+
+DECLARE_DELEGATE_OneParam(FCoreAssetManagerStartupJobSubstemProgress, float /*NewProgress*/);
+
+/** Handles reporting from streamable handles */
+struct FCoreAssetManagerStartupJob
+{
+	FCoreAssetManagerStartupJobSubstemProgress SubstepProgressDelegate;
+	TFunction<void(const FCoreAssetManagerStartupJob&, TSharedPtr<FStreamableHandle>&)> JobFunc;
+	FString JobName;
+	float JobWeight;
+	mutable  double LastUpdate = 0;
+	
+	/** Simple job that all synchronous */
+	FCoreAssetManagerStartupJob(const FString& InJobName, const TFunction<void(const FCoreAssetManagerStartupJob&, TSharedPtr<FStreamableHandle>&)>& InJobFunc, float InJobWeight)
+		: JobFunc(InJobFunc)
+	, JobName(InJobName)
+	, JobWeight(InJobWeight)
+	{}
+	
+	/** Perform actual loading, will return a handle if it created one */
+	TSharedPtr<FStreamableHandle> DoJob() const;
+	
+	void UpdateSubstepProgress(float NewProgress) const
+	{
+		SubstepProgressDelegate.ExecuteIfBound(NewProgress);
+	}
+	
+	void UpdateSubstepProgressFromStreamable(TSharedRef<FStreamableHandle> StreamableHandle) const
+	{
+		if (SubstepProgressDelegate.IsBound())
+		{
+			double Now = FPlatformTime::Seconds();
+			if (Now - LastUpdate > 1.0 / 60)
+			{
+				SubstepProgressDelegate.Execute(StreamableHandle->GetProgress());
+				LastUpdate = Now;
+			}
+		}
+	}
+};
+
