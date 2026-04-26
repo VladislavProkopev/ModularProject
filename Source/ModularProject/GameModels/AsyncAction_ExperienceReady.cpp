4d1f2fd4298685407181527710c1e55838350cfd
VladislavProkopev <vladislavprokopevcplusplus@gmail.com>
2026-04-26 17:39:48 +0500

    AsyncAction_ExperienceReady.cpp Base created

---
+++ Source/ModularProject/GameModels/AsyncAction_ExperienceReady.cpp
@@ -0,0 +1,89 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AsyncAction_ExperienceReady.h"
+#include "Engine/Engine.h"
+#include "Engine/World.h"
+#include "CoreExperienceManagerComponent.h"
+#include "TimerManager.h"
+
+UAsyncAction_ExperienceReady::UAsyncAction_ExperienceReady(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+}
+
+UAsyncAction_ExperienceReady* UAsyncAction_ExperienceReady::WaitForExperienceReady(const UObject* WorldContextObject)
+{
+	UAsyncAction_ExperienceReady* Action = nullptr;
+
+	if (UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject,EGetWorldErrorMode::LogAndReturnNull))
+	{
+		Action = NewObject<UAsyncAction_ExperienceReady>();
+		Action->WorldPtr = World;
+		Action->RegisterWithGameInstance(World);
+	}
+	
+	return Action;
+}
+
+void UAsyncAction_ExperienceReady::Activate()
+{
+	if (UWorld* World = WorldPtr.Get())
+	{
+		if (AGameStateBase* GameState = World->GetGameState())
+		{
+			Step2_ListenToExperienceLoading(GameState);
+		}
+		else
+		{
+			World->GameStateSetEvent.AddUObject(this,&ThisClass::Step1_HandleGameStateSet);
+		}
+	}
+	else
+	{
+		//No world so we'll never finish naturally
+		SetReadyToDestroy();
+	}
+}
+
+void UAsyncAction_ExperienceReady::Step1_HandleGameStateSet(AGameStateBase* GameState)
+{
+	if (UWorld* World = WorldPtr.Get())
+	{
+		World->GameStateSetEvent.RemoveAll(this);
+	}
+	Step2_ListenToExperienceLoading(GameState);
+}
+
+void UAsyncAction_ExperienceReady::Step2_ListenToExperienceLoading(AGameStateBase* GameState)
+{
+	check(GameState);
+	UCoreExperienceManagerComponent* ExperienceComponent = GameState->FindComponentByClass<UCoreExperienceManagerComponent>();
+	check(ExperienceComponent);
+
+	if (ExperienceComponent->IsExperienceLoaded())
+	{
+		UWorld* World = GameState->GetWorld();
+		check(World);
+		
+		//The experience happend to be already loaded, but still delay a frame to
+		//make sure people don't write stuff that relies on always being true
+		//@TODO Consider not delaying spawned stuff/any time after the loading screen has dropped?
+		//@TODO Maybe just inject a random 0-1s delay in the experience loadi itself?
+		World->GetTimerManager().SetTimerForNextTick(FTimerDelegate::CreateUObject(this,&ThisClass::Step4_BroadcastReady));
+	}
+	else
+	{
+		ExperienceComponent->CallOrRegister_OnExperienceLoaded(FOnCoreExperienceLoaded::FDelegate::CreateUObject(this,&ThisClass::Step3_HandleExperienceLoaded));
+	}
+}
+
+void UAsyncAction_ExperienceReady::Step3_HandleExperienceLoaded(const UCoreExperienceDefinition* CurrentExperience)
+{
+	Step4_BroadcastReady();
+}
+
+void UAsyncAction_ExperienceReady::Step4_BroadcastReady()
+{
+	OnReady.Broadcast();
+	SetReadyToDestroy();
+}
---
+++ Source/ModularProject/GameModels/AsyncAction_ExperienceReady.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "AsyncAction_ExperienceReady.generated.h"
+
+class AGameStateBase;
+class UCoreExperienceDefinition;
+class UWorld;
+struct FFrame;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FExperienceReadyAsyncDelegate);
+
+/**
+ * Asynctonously wait for the game state to be ready and valid and then calls the OneReady event. Will call OnReady
+ * immediately if the game state is valid already
+ */
+UCLASS()
+class UAsyncAction_ExperienceReady : public UBlueprintAsyncActionBase
+{
+	GENERATED_UCLASS_BODY()
+	
+public:
+	//Waits for the experience to be determined and loaded
+	UFUNCTION(BlueprintCallable,meta=(WorldContext = "WorldContextObject",BlueprintInternalUseOnly="true"))
+	static UAsyncAction_ExperienceReady* WaitForExperienceReady(const UObject* WorldContextObject);
+	
+	virtual void Activate() override;
+	
+	//Called when the experience has been determined and is ready/loaded
+	UPROPERTY(BlueprintAssignable)
+	FExperienceReadyAsyncDelegate OnReady;
+	
+private:
+	void Step1_HandleGameStateSet(AGameStateBase* GameState);
+	void Step2_ListenToExperienceLoading(AGameStateBase* GameState);
+	void Step3_HandleExperienceLoaded(const UCoreExperienceDefinition* CurrentExperience);
+	void Step4_BroadcastReady();
+	
+	TWeakObjectPtr<UWorld> WorldPtr;
+};
