80f9b6647f64b9a4e729239d5df8859083941def
VladislavProkopev <vladislavprokopevcplusplus@gmail.com>
2026-04-26 17:40:32 +0500

    CoreExperienceActionSet.cpp base created

---
+++ Source/ModularProject/GameModels/CoreExperienceActionSet.cpp
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CoreExperienceActionSet.h"
+#include "GameFeatureAction.h"
+
+#if WITH_EDITOR
+#include "Misc/DataValidation.h"
+#endif
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(CoreExperienceActionSet)
+
+#define LOCTEXT_NAMESPACE "CoreSystem"
+
+UCoreExperienceActionSet::UCoreExperienceActionSet()
+{
+}
+
+#if WITH_EDITOR
+EDataValidationResult UCoreExperienceActionSet::IsDataValid(class FDataValidationContext& Context) const
+{
+	EDataValidationResult Result = CombineDataValidationResults(Super::IsDataValid(Context),EDataValidationResult::Valid);
+	
+	int32 EntryIndex = 0;
+	for (const UGameFeatureAction* Action : Actions)
+	{
+		if (Action)
+		{
+			EDataValidationResult ChildResult = Action->IsDataValid(Context);
+			Result = CombineDataValidationResults(Result,ChildResult);
+		}
+		else
+		{
+			Result = EDataValidationResult::Invalid;
+			Context.AddError(FText::Format(LOCTEXT("ActionEntryIsNull", "Null entry at index {0} in Actions"),FText::AsNumber(EntryIndex)));
+		}
+		++EntryIndex;
+	}
+	return Result;
+}
+#endif
+
+#if WITH_EDITORONLY_DATA
+void UCoreExperienceActionSet::UpdateAssetBundleData()
+{
+	Super::UpdateAssetBundleData();
+	for (UGameFeatureAction* Action : Actions)
+	{
+		if (Action)
+		{
+			Action->AddAdditionalAssetBundleData(AssetBundleData);
+		}
+	}
+}
+#endif
+
---
+++ Source/ModularProject/GameModels/CoreExperienceActionSet.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Engine/DataAsset.h"
+#include "CoreExperienceActionSet.generated.h"
+
+class UGameFeatureAction;
+
+/**
+ * Definition of a set of actions to perform as part of entering an experience
+ */
+UCLASS(BlueprintType,NotBlueprintable)
+class UCoreExperienceActionSet : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+public:
+	UCoreExperienceActionSet();
+	
+	//~ UObject interface
+#if WITH_EDITOR
+	virtual EDataValidationResult IsDataValid(class FDataValidationContext& Context) const override;
+#endif
+	//~ End UObject interface
+	
+	//~ UPrimaryDataAsset
+#if WITH_EDITORONLY_DATA
+	virtual void UpdateAssetBundleData() override;
+#endif
+	//~ End UPrimaryDataAsset
+	
+	//List of actions to perform as this experience is loaded/activated/deactivated/unloaded
+	UPROPERTY(EditAnywhere,Instanced,Category="Actions to Perform")
+	TArray<TObjectPtr<UGameFeatureAction>> Actions;
+	
+	//List of Game Feature Plugins this experience wants to have active
+	UPROPERTY(EditAnywhere,Category="Feature Dependencies")
+	TArray<FString> GameFeaturesToEnable;
+};
