c594bac6e872dcdfd1cf0933e043297e4605bc23
VladislavProkopev <vladislavprokopevcplusplus@gmail.com>
2026-04-26 17:42:30 +0500

    CoreLogChannels.cpp base created

---
+++ Source/ModularProject/CoreLogChannels.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "Logging/LogMacros.h"
+
+class UObject;
+
+MODULARPROJECT_API DECLARE_LOG_CATEGORY_EXTERN(LogProjectCore,Log,All);
+MODULARPROJECT_API DECLARE_LOG_CATEGORY_EXTERN(LogCoreExperience,Log,All);
+MODULARPROJECT_API DECLARE_LOG_CATEGORY_EXTERN(LogCoreAbilitySystem,Log,All);
+MODULARPROJECT_API DECLARE_LOG_CATEGORY_EXTERN(LogCoreTeams,Log,All);
+
+MODULARPROJECT_API FString GetClientServerContextString(UObject* ContextObject = nullptr);
+
