87539ac7dbe0d4e6d2188da7377165a590011846
VladislavProkopev <vladislavprokopevcplusplus@gmail.com>
2026-04-26 17:41:48 +0500

    CoreLoadingProcessTask.cpp base created

---
+++ Plugins/CoreCommonLoadingScreen/Source/CoreCommonLoadingScreen/CoreLoadingProcessTask.cpp
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CoreLoadingProcessTask.h"
+#include "Engine/Engine.h"
+#include "Engine/World.h"
+#include "CoreLoadingScreenManager.h"
+#include "UObject/ScriptInterface.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(CoreLoadingProcessTask)
+
+/*static*/UCoreLoadingProcessTask* UCoreLoadingProcessTask::CreateCoreLoadingProcessTask(UObject* WorldContextObject,
+	const FString& ShowLoadingScreenReason)
+{
+	UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject,EGetWorldErrorMode::LogAndReturnNull);
+	UGameInstance* GameInstance = World ? World->GetGameInstance() : nullptr;
+	UCoreLoadingScreenManager* LoadingScreenManager = GameInstance ? GameInstance->GetSubsystem<UCoreLoadingScreenManager>() : nullptr;
+
+	if (LoadingScreenManager)
+	{
+		UCoreLoadingProcessTask* NewLoadingTask = NewObject<UCoreLoadingProcessTask>(LoadingScreenManager);
+		NewLoadingTask->SetShowLoadingScreenReason(ShowLoadingScreenReason);
+		
+		LoadingScreenManager->RegisterLoadingProcessor(NewLoadingTask);
+		
+		return NewLoadingTask;
+	}
+	return nullptr;
+}
+
+void UCoreLoadingProcessTask::Unregister()
+{
+	UCoreLoadingScreenManager* LoadingScreenManager = Cast<UCoreLoadingScreenManager>(GetOuter());
+	LoadingScreenManager->UnregisterLoadingProcessor(this);
+}
+
+void UCoreLoadingProcessTask::SetShowLoadingScreenReason(const FString& InReason)
+{
+	Reason = InReason;
+}
+
+bool UCoreLoadingProcessTask::ShouldShowLoadingScreen(FString& OutReason)
+{
+	OutReason = Reason;
+	return true;
+}
---
+++ Plugins/CoreCommonLoadingScreen/Source/CoreCommonLoadingScreen/CoreLoadingProcessTask.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "LoadingProcessInterface.h"
+#include "UObject/Object.h"
+#include "CoreLoadingProcessTask.generated.h"
+
+#define UE_API CORECOMMONLOADINGSCREEN_API
+
+struct FFrame;
+
+/**
+ * 
+ */
+UCLASS(MinimalAPI,BlueprintType)
+class UCoreLoadingProcessTask : public UObject , public ILoadingProcessInterface
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable,meta=(WorldContext="WorldContextObject"))
+	static UE_API UCoreLoadingProcessTask* CreateCoreLoadingProcessTask(UObject* WorldContextObject,const FString& ShowLoadingScreenReason);
+	
+	UCoreLoadingProcessTask(){}
+	
+	UFUNCTION(BlueprintCallable)
+	UE_API void Unregister();
+	
+	UFUNCTION(BlueprintCallable)
+	UE_API void SetShowLoadingScreenReason(const FString& InReason);
+	
+	UE_API virtual bool ShouldShowLoadingScreen(FString& OutReason);
+	
+	FString Reason;
+};
+
+#undef UE_API
